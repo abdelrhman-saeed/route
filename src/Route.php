ad9905360886f6f79571de7eee37d284167a79ff
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2024-10-01 02:24:24 +0300

    changed the Route::handle() method to take the routes file path as an argument

--- src/Route.php
+++ src/Route.php
@@ -23,14 +23,38 @@ use Symfony\Component\HttpFoundation\Request;
 
 class Route
 {
+    /**
+     * $headUri, $currentUri hold a reference to the current URI object
+     * @var 
+     */
     private static ?URI $headUri , $currentUri = null;
 
+    /**
+     * $supportedHttpMethods defines the supported https methods by the package
+     * @var array
+     */
+    public static array $supportedHttpMethods = ['get', 'post', 'put', 'patch', 'delete'];
+
+    /**
+     * __callstatic used to make static method calls using
+     * the supportedHttpMethods as the methods name instead of duplicating the same code for each method
+     * 
+     * @param string $method
+     * @param mixed $args
+     * @throws \AbdelrhmanSaeed\Route\Exceptions\NotSupportedHttpMethodException
+     * @return \AbdelrhmanSaeed\Route\URI\URIConstraints
+     */
     public static function __callstatic(string $method, mixed $args): URIConstraints
     {
-        if (! in_array(strtolower($method), URI::$supportedHttpMethods)) {
+        /**
+         * if the name of the called static method is not one of the supported http methods
+         * a NotSupportedHttpMethodException exception will be thrown
+         */
+        if (! in_array(strtolower($method), self::$supportedHttpMethods)) {
             throw new NotSupportedHttpMethodException("the method '$method' is not supported!");
         }
 
+        // setting up an URI object and injecting a URIAction and URIConstraints objects to it
         $uri = new URI(
                 $args[0],
                 $method,
@@ -40,6 +64,11 @@ class Route
         $uri->setUriConstraints(
                 new URIConstraints($uri));
 
+        /**
+         * making a chaing of URI objects apply the chain of responsibility design pattern
+         * each URI object in the chain will try to handle the request
+         * till one object handles it
+         */
         if (is_null(self::$currentUri))
         {
             self::$headUri = self::$currentUri = $uri;
@@ -50,11 +79,51 @@ class Route
         self::$currentUri->setNext($uri);
         self::$currentUri = $uri;
 
+        /**
+         * returning the URIConstraints object that is linked the URI object
+         * so the user be able to define some constraints on the route segments
+         */
         return $uri->getUriConstraints();
     }
 
-    public static function handle(): void
+    public static function setup(string $routesFile): void
     {
+        require $routesFile;
         self::$headUri?->handle(Request::createFromGlobals());
     }
+
+    // public static function resource(
+    //     string $url, string $controller, bool $nested = false, bool $shallowed = false): void
+    // {
+        
+
+        
+    //     $identifiedResourceUrl = "$url/{$url}";
+
+    //     /**
+    //      * get method
+    //      */
+    //     self::get($url, [$controller, 'index']);
+    //     self::get($identifiedResourceUrl, [$controller, 'show']);
+
+    //     /**
+    //      * post method
+    //      */
+    //     self::post($url, [$controller, 'save']);
+
+    //     /**
+    //      * put
+    //      */
+    //     self::put($identifiedResourceUrl, [$controller, 'update']);
+
+    //     /**
+    //      * patch
+    //      */
+    //     self::patch($identifiedResourceUrl, [$controller, 'update']);
+
+    //     /**
+    //      * delete
+    //      */
+    //     self::delete($identifiedResourceUrl, [$controller, 'delete']);
+    // }
 }
