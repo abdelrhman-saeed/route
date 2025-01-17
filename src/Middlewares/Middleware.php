98dcb9578c7422b23c36bebbe43e438ff0635944
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2025-01-17 22:31:06 +0200

    docs: document the abstract Middleware class file

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -2,26 +2,43 @@
 
 namespace AbdelrhmanSaeed\Route\Middlewares;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\{Request, Response};
 
 
 abstract class Middleware
 {
+    /**
+     * a pointer to the next middleware
+     * 
+     * a pointer to the next middleware
+     * in case if the middleware will pass the handled request
+     * to the next one
+     * @var null|Middleware
+     */
     private ?Middleware $next = null;
 
-    public function setNext(Middleware $next): self
-    {
+    /**
+     * @param \AbdelrhmanSaeed\Route\Middlewares\Middleware $next
+     * @return \AbdelrhmanSaeed\Route\Middlewares\Middleware
+     */
+    public function setNext(Middleware $next): self {
         $this->next = $next;
         return $this;
     }
 
-    public function getNext(): Middleware
-    {
+    /**
+     * @return Middleware|null
+     */
+    public function getNext(): ?Middleware {
         return $this->next;
     }
 
-    public function handle(Request $request): void
-    {
-        $this->next?->handle($request);
+    /**
+     * Summary of handle
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response|null
+     */
+    public function handle(Request $request): ?Response {
+        return $this->next?->handle($request);
     }
 }
